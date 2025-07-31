cb24ff7b0f0f9ae45f868264754abbfbcc029916
Allen <allen@Mac.attlocal.net>
2025-07-31 09:43:25 -0700

    .env file fix:

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,13 +1,7 @@
 /// <reference types="vite/client" />
 
 interface ImportMetaEnv {
-  readonly VITE_API_HOST: string
-  readonly VITE_ITINERARY_WORKFLOW_ID: string
-  readonly VITE_ITINERARY_WORKFLOW_SECRET: string
-  readonly VITE_ITINERARY_OPTIMIZATION_PROFILE: string
-  readonly VITE_CHAT_WORKFLOW_ID: string
-  readonly VITE_CHAT_WORKFLOW_SECRET: string
-  readonly VITE_CHAT_MODEL_OVERRIDE: string
+  readonly VITE_BACKEND_URL: string
 }
 
 interface ImportMeta {
